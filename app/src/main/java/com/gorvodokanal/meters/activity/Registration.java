06c717c140880ca6abca6fd21970e60b3f3a4a74
Ivan <ivan701-1992@mail.ru>
2020-09-11 06:59:55 +0700

    Change all fragment

--- app/src/main/java/com/gorvodokanal/meters/activity/Registration.java
+++ app/src/main/java/com/gorvodokanal/meters/activity/Registration.java
@@ -3,14 +3,26 @@ package com.gorvodokanal.meters.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gorvodokanal.R;
 
+import java.util.ArrayList;
+
 public class Registration extends AppCompatActivity {
+    EditText kod;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ((EditText) findViewById(R.id.kod)).getText();
+
+
+
     }
+
 }
