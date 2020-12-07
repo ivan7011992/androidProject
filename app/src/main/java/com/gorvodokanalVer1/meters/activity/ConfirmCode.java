a009d68a115e873f7992a837a03a6191d1fae755
ivasilev <ivan701-1992@mail.ru>
2020-12-07 16:33:35 +0700

    BindingLs

---
+++ app/src/main/java/com/gorvodokanalVer1/meters/activity/ConfirmCode.java
@@ -0,0 +1,17 @@
+package com.gorvodokanalVer1.meters.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gorvodokanalVer1.R;
+
+public class ConfirmCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_code);
+
+    }
+}
